fe81a05cf6534e2f29647d1658d7e1c4cdeab766
apiel <alexandre.piel@gmail.com>
2018-09-24 11:10:22 +0200

    Update authChecker.ts
    Some though about authorization

--- backend/src/authChecker.ts
+++ backend/src/authChecker.ts
@@ -3,6 +3,17 @@ import { get } from 'lodash';
 
 import { Context } from "./context.interface";
 
+// here we could have a session logic
+// if we want to always check that the user
+// is still valid (not banned)
+// instead to rely on the 12h validity of the JWT
+
+// We could also use a JWT valid only for 5min (or less)
+// and then have a second token (checking for validity in db) to refresh the JWT
+// But there would still be 5min
+
+// Else create a JWT blacklist with redis or postgres hash table
+
 const authChecker: AuthChecker<Context> = ({ context: { user } }, roles) => {
   // console.log('authChecker', user, roles);
   if (roles.length === 0) {
