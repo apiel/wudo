efaa698f4bded4ff10c053f1387785a8984794e3
Alexandre Piel <alexandre.piel@gmail.com>
2018-09-27 20:08:14 +0200

    use react adopt

--- react-app/src/component/follower/Followers.js
+++ react-app/src/component/follower/Followers.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Query } from 'react-apollo';
 import get from 'lodash/get';
+import { adopt } from 'react-adopt';
 
 import GET_FOLLOWERS from '../../gql/query/getFollowers';
 import GET_ME from '../../gql/query/getMe';
@@ -8,45 +9,35 @@ import GET_ME from '../../gql/query/getMe';
 import AppBarBack from '../appBar/AppBarBack';
 import FollowerItem from './FollowerItem';
 
-// we might display all the tags we created
-// so we should get tags as well
-// loop tags
+const Composed = adopt({
+    me: ({ render }) => <Query query={GET_ME}>{render}</Query>,
+    followers: ({ render }) => <Query query={GET_FOLLOWERS}>{render}</Query>,
+});
+
 const Followers = () => (
     <div>
         <AppBarBack title='Followers' />
-        <Query
-            query={GET_ME}
-        >
-            {({ loading, error, data: { getMe: { tags } } }) => {
-                if (loading) return <p>Loading tags...</p>;
-                if (error) return <p>Error :(</p>;
+        <Composed>
+            {({ me, followers}) => {
+                if (me.loading || followers.loading) return <p>Loading...</p>;
+                if (me.error || followers.error) return <p>Error :(</p>;
 
-                // console.log('tags', tags);
+                const { data: { getFollowers } } = followers;
+                const { data: { getMe: { tags } } } = me;
 
-                return (
-                    <Query
-                        query={GET_FOLLOWERS}
-                    >
-                        {({ loading, error, data: { getFollowers } }) => {
-                            if (loading) return <p>Loading followers...</p>;
-                            if (error) return <p>Error :(</p>;
+                const tagsFollowedByUser = get(getFollowers, 'tagsFollowedByUser', []);
+                const users = get(getFollowers, 'users', []);
+                // console.log('getFollowers', getFollowers);
 
-                            const tagsFollowedByUser = get(getFollowers, 'tagsFollowedByUser', []);
-                            const users = get(getFollowers, 'users', []);
-                            // console.log('getFollowers', getFollowers);
-
-                            return tags.map(({ idTag, name }) => {
-                                // console.log('tag', idTag, name);
-                                const index = tagsFollowedByUser.findIndex(follower => follower.idTag === idTag);
-                                const followers = index === -1 ? [] : tagsFollowedByUser[index].users;
-                                // console.log('followers', followers);
-                                return (<FollowerItem key={idTag} name={name} users={users} followers={followers} />);
-                            });
-                        }}
-                    </Query>
-                );
+                return tags.map(({ idTag, name }) => {
+                    // console.log('tag', idTag, name);
+                    const index = tagsFollowedByUser.findIndex(follower => follower.idTag === idTag);
+                    const followers = index === -1 ? [] : tagsFollowedByUser[index].users;
+                    // console.log('followers', followers);
+                    return (<FollowerItem key={idTag} name={name} users={users} followers={followers} />);
+                });
             }}
-        </Query>
+        </Composed>
     </div>
 );
 
